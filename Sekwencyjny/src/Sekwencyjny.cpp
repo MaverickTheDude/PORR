4836c1566f5b6aa15215edc28c272fb0229da6ae
MaverickTheDude <38852811+MaverickTheDude@users.noreply.github.com>
2019-10-31 15:06:18 +0100

    Biblioteki
     - Sekw2 - zawiera przygotowana biblioteke Armadillo oraz niezbedne ustawienia, zeby zadzialala.
     - Sekwencyjny - poczatek projektu.

--- Sekwencyjny/src/Sekwencyjny.cpp
+++ Sekwencyjny/src/Sekwencyjny.cpp
@@ -1,15 +1,40 @@
 //============================================================================
 // Name        : Sekwencyjny.cpp
-// Author      : 
+// Author      : PM
 // Version     :
-// Copyright   : Your copyright notice
+// Copyright   : Homework and training
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
 #include <iostream>
+#include <Eigen>
 using namespace std;
+using namespace Eigen;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+
+	  Vector3d v(1,2,3);
+	  Vector3d w(0,1,2);
+
+	  cout << "Dot product: " << v.dot(w) << endl;
+	  double dp = v.adjoint()*w; // automatic conversion of the inner product to a scalar
+	  cout << "Dot product via a matrix product: " << dp << endl;
+	  cout << "Cross product:\n" << v.cross(w) << endl;
+
+	  cout << "Here is the matrix A:\n" << v.adjoint() << endl;
+
+	  std::cout << "\n\n" << std::endl;
+
+
+
+	  std::cout << "\n\n" << std::endl;
+
+	   Matrix2f A, b;
+	   A << 2, -1, -1, 3;
+	   b << 1, 2, 3, 1;
+	   cout << "Here is the matrix A:\n" << A << endl;
+	   cout << "Here is the right hand side b:\n" << b << endl;
+	   Matrix2f x = A.ldlt().solve(b);
+	   cout << "The solution is:\n" << x << endl;
+	   return 0;
 }
---
+++ test/src/test.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : test.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <mutex>
+
+using namespace std;
+
+int main() {
+
+	mutex zmienna;
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
