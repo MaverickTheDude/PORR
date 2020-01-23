43dac5d9cd4b9a2e83865e3f8a230974b648b1b3
MaverickTheDude <38852811+MaverickTheDude@users.noreply.github.com>
2020-01-23 10:23:32 +0100

    Matlab CPU Sequential finish

--- Matlab_GPU/main.m
+++ Matlab_GPU/main.m
@@ -1,20 +1,25 @@
 addpath('inputs','utilities','misc')
 %% nasz main
-Nbodies = 4;
+Nbodies = 2048;
 [tiers, tiersInfo] = getTiers(Nbodies);
-L = 0.4; % [m]
-mass = 0.5;
+L = 0.04; % [m]
+mass = 0.05;
 Icm = 1/12*mass*L^2;
 M = diag([mass mass Icm]);
 sC1 = [-L/2; 0]; sC2 = [L/2; 0];
-p0 = dq0top0(input{7}.q0, input{7}.dq0, input);
+q0 = zeros(Nbodies,1); q0(1) = pi/6;
+p0 = zeros(Nbodies,1); 
+dt = 0.01; Tk = dt;
+% p0 = dq0top0(input{7}.q0, input{7}.dq0, input);
 inputTMP = struct('Nbodies',Nbodies,'q0',q0,'p0',p0,'dt',dt,'Tk',Tk,...
     'tiers',tiers,'tiersInfo',tiersInfo,'M',M,'sC1_loc',sC1,'sC2_loc',sC2);
 
 T = 0:inputTMP.dt:inputTMP.Tk;
+tic
 Ytab = GPU_RK45(inputTMP);
-pTab = data(1:Nbodies,:);
-qTab = data(Nbodies+1:end,:);
+t = toc
+pTab = Ytab(1:Nbodies,:);
+qTab = Ytab(Nbodies+1:end,:);
 
 %% compare
 input = FI_HDCA_4links();
@@ -26,4 +31,8 @@ stateHDCA = HDCASolver(input);
 stateH = joint2absolute(stateHDCA, input);
 calculateEnergy(stateH, input);
 
+%% wykresy
+x = [512 1024 1500 2048]
+y = [0.911 1.807 2.7 3.586]
 
+fig; plot(x,y,'o'); grid on
